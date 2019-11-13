f8da18002b7e2ad3d2b7b785107afa8d74f154d2
Paul Dreik <github@pauldreik.se>
2019-11-13 13:16:27 +0100

    fix the build flags and invocation

--- build_fuzzer.sh
+++ build_fuzzer.sh
@@ -6,18 +6,18 @@ export CC=afl-gcc
 export CXX=afl-g++
 export CXXFLAGS="-fsanitize=address,undefined -g"
 export CFLAGS="-fsanitize=address,undefined -g"
-
+export LDFLAGS="-fsanitize=address,undefined"
 
 if [ ! -e configure ] ; then
    autoreconf
 fi
 
 
-./configure 
+./configure --enable-debug
 
 make
 
 
 mkdir -p afl-in afl-out
-echo afl-fuzz -i afl-in -o afl-out -- src/tnef -K @@
+echo afl-fuzz -m9999999999 -i afl-in -o afl-out -- src/tnef -K
 
